adadc976f384d29500c2c0cd74cda81ba376e785
Gonzalo Barrera Borla <gonzalobb@gmail.com>
2015-04-20 01:00:48 -0300

    Soluciones para los problemasde instalacion de gemas mas comunes.

--- pasos/005_laburo.sh
+++ pasos/005_laburo.sh
@@ -10,3 +10,12 @@ echo "# Alias laborales:" >> ~/.bash_aliases
 echo "alias spr='cd ~/lavoro/sac-platform-reporting'" >> ~/.bash_aliases
 echo "alias spa='cd ~/lavoro/sac-platform-analyses'" >> ~/.bash_aliases
 . ~/.bashrc
+
+# Para instalar nokogiri-1.5.5, se necesitan algunos paquetes extras. Antes de correr `bundle install`, ejecutar:
+sudo apt-get install libxml2-dev libxslt-dev 
+
+# Para instalar eventmachine-1.0.3, hay que tener g++
+sudo apt-get install g++
+
+# Para instalar mysql2, hay que tener algun cliente de SQL Usamos el default de 14.04 (mysql-5.5) y las librerias de desarrollo
+sudo apt-get install mysql-server mysql-client libmysqlclient-dev
