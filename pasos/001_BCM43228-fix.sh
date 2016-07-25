3cc9a39bbb4a54760249fb52c7cbc3c13cead96f
Gonzalo Barrera Borla <gonzalobb@gmail.com>
2016-07-25 17:39:55 -0300

    Limpiar y explicar fix para placa de red en Ubuntu 12.04+ para Dell Latitude E6220 (Ohana)

--- pasos/001_BCM43228-fix.sh
+++ pasos/001_BCM43228-fix.sh
@@ -1,2 +1,9 @@
-sudo apt-get purge bcmwl-kernel-source broadcom-sta-common broadcom-sta-source b43-fwcutter firmware-b43-installer
-sudo apt-get install bcmwl-kernel-source
+#!/bin/bash
+
+# Problema : Instalacion de Ubuntu 12.04+ en Dell Latitude E6220 no reconozca la placa wifi
+# Solucion: 	Primero, purgar todos los drivers que por default Ubuntu incluye para placas Broadcom
+# 		Segundo, instalar unicamente el driver en particular que si funciona
+# Notas: Probablemente requiera reniciar para surtir efecto? No estoy seguro
+
+sudo apt purge -y bcmwl-kernel-source broadcom-sta-common broadcom-sta-source b43-fwcutter firmware-b43-installer
+sudo apt install -y bcmwl-kernel-source
